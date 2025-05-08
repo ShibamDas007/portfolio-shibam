7bc2c437d94417f4be3c2da0927de8ed8c7cc51f
ShibamDas007 <shibamdas575@gmail.com>
2025-05-08 18:19:35 +0530

    Updated Next.js project with Tailwind CSS

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,14 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Fira_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+});
+
+const firaMono = Fira_Mono ({
+  subsets: ['latin'],
+  weight: ['400', '700'],
 });
 
 const geistMono = Geist_Mono({
@@ -20,7 +25,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${firaMono.className}`}
       >
         {children}
       </body>
